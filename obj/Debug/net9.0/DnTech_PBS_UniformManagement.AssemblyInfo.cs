504cac730e80a1bc5f3138bfc8ed462307274b35
Danny Francisco Castillo Solano <93727378+Dan4541@users.noreply.github.com>
2025-11-26 20:30:41 -0600

    Adding the git ignore file

--- obj/Debug/net9.0/DnTech_PBS_UniformManagement.AssemblyInfo.cs
+++ obj/Debug/net9.0/DnTech_PBS_UniformManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DnTech_PBS_UniformManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b7ed9cea996b39bdcff6b4c8b6af9c86d20d3d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("DnTech_PBS_UniformManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DnTech_PBS_UniformManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
